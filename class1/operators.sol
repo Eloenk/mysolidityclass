bc7c39de1477d88e2ab32598b4a67b6fd99de28e
Eloenk <64978907+Eloenk@users.noreply.github.com>
2025-01-14 06:53:40 -0800

    basic logic operators

---
+++ class1/operators.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.3;
+
+contract Opera{
+    function exp(uint a, uint b) external pure returns(uint) {
+        return a ** b;
+    
+    }
+}
