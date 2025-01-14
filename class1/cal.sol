fd061bfcf2c96a496c9fe4e947ab139c91ce0942
Eloenk <64978907+Eloenk@users.noreply.github.com>
2025-01-14 06:23:18 -0800

    Update cal.sol

--- class1/cal.sol
+++ class1/cal.sol
@@ -3,5 +3,11 @@
 pragma solidity ^0.8.3;
 
 contract Cal {
-        
+        uint num = 20;
+        uint numj = 5;
+
+        function adder() public view return(uint ans){
+        ans = num + numj;
+        return ans;
+    }
     }
