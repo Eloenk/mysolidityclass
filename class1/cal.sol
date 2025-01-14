2e0be0c95b68f01c8179d77ff8d75febaf159dd3
Eloenk <64978907+Eloenk@users.noreply.github.com>
2025-01-14 06:47:43 -0800

    Update cal.sol

--- class1/cal.sol
+++ class1/cal.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.3;
 
 contract Cal {
         function adder(uint a, uint b) external pure returns(uint) {
-        returns a+b;
+        return a+b;
     }
         function subtra(uint a, uint b) external pure returns(uint){
-        returns a - b;
+        return a - b;
+    }
+        function mul(uint a, uint b) external pure returns(uint) {
+        return a * b;
+    }
+        function div(uint a, uint b) external pure returns(uint){
+        return a / b;
     }
 }
